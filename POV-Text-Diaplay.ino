21d2aedd5d315d6297a34ff8060d58c7cc28413f
Ravi Kumar Prasad <ravikp7@outlook.com>
2017-04-09 23:40:15 +0530

    pov code for node-mcu
    GPIO 1-5 are used for 5 LEDs.

--- POV-Text-Diaplay.ino
+++ POV-Text-Diaplay.ino
@@ -36,17 +36,17 @@ int dotTime;
 void setup()
 {
   // Setting the ports of the leds to OUTPUT
+  pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
  
   // Defining the space between the letters (ms)
   letterSpace = 6;
   // Defining the time dots appear (ms)
   dotTime = 3;
- 
+ Serial.begin(115200);
 }
 
 
@@ -60,28 +60,28 @@ void displayLetter(int letter[])
   // Printing the first y column of the letter
   for (y=0; y<5; y++)
   {
-    digitalWrite(y+2, letter[y]);
+    digitalWrite(y+1, letter[y]);
   }
   delay(dotTime);
  
   // Printing the second y column of the letter
   for (y=0; y<5; y++)
   {
-    digitalWrite(y+2, letter[y+5]);
+    digitalWrite(y+1, letter[y+5]);
   }
   delay(dotTime);
  
   // Printing the third y column of the letter
   for (y=0; y<5; y++)
   {
-    digitalWrite(y+2, letter[y+10]);
+    digitalWrite(y+1, letter[y+10]);
   }
   delay(dotTime);
  
   // Printing the space between the letters
   for (y=0; y<5; y++)
   {
-    digitalWrite(y+2, 0);
+    digitalWrite(y+1, 0);
   }
   delay(letterSpace);
 }
@@ -93,7 +93,7 @@ void displayLetter(int letter[])
 void loop()
 {
   // Print text here 
- displayText("ULTIMATE HACKER");
+ displayText("ULTIMATE_HACKER");
   
 }
 
@@ -103,10 +103,43 @@ void loop()
 // Function to display Text
 void displayText(char str[])
 {
-    int len = sizeof(str); // define str[] length
+    int len = strlen(str); // define str[] length
     int i;
     for(i = 0; i<len; i++)
     {
-        displayLetter(str[i]);
+  
+        switch((int)str[i]){
+
+          case 95: displayLetter(_);
+          case 65: displayLetter(A);
+          case 66: displayLetter(B);
+          case 67: displayLetter(C);
+          case 68: displayLetter(D);
+          case 69: displayLetter(E);
+          case 70: displayLetter(F);
+          case 71: displayLetter(G);
+          case 72: displayLetter(H);
+          case 73: displayLetter(I);
+          case 74: displayLetter(J);
+          case 75: displayLetter(K);
+          case 76: displayLetter(L);
+          case 77: displayLetter(M);
+          case 78: displayLetter(N);
+          case 79: displayLetter(O);
+          case 80: displayLetter(P);
+          case 81: displayLetter(Q);
+          case 82: displayLetter(R);
+          case 83: displayLetter(S);
+          case 84: displayLetter(T);
+          case 85: displayLetter(U);
+          case 86: displayLetter(V);
+          case 87: displayLetter(W);
+          case 88: displayLetter(X);
+          case 89: displayLetter(Y);
+          case 90: displayLetter(Z);
+    
+          
+          }
+          
     }     
 }
